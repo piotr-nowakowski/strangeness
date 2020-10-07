89919227152c97d67af427f80f4fc0953dd460cf
piotr-nowakowski <pnowakow@mion.elka.pw.edu.pl>
2020-10-07 23:57:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 #echo "$@" > /tmp/log.txt
-echo "${@:2}" > /tmp/log2.txt
-root --notebook "${@:2}"
+#echo "${@:2}" > /tmp/log2.txt
+root --notebook "${@:2}" --notebook-dir=/home/jovyan/
 
 #exec "$@"
